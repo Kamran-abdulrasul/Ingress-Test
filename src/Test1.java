bf6e2118f6df38f644c28bc92092581c8466e9f3
Kamran Abdulrasul <kamran.abdulrasulov@gmail.com>
2022-09-28 15:25:20 +0400

    Added new class

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{4, 7, 45, 8};
+
+        for (int i =0; i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
